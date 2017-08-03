219330ed04909a3dc3c5e6e2043b88fe98c00c06
Regner Blok-Andersen <shadowdf@gmail.com>
2017-08-03 09:22:59 -0700

    no message

--- handlers/ingest_market_orders.go
+++ handlers/ingest_market_orders.go
@@ -5,15 +5,28 @@ import (
 "github.com/regner/albiondata-backend/dispatcher"
 )
 
-type ingestPostRequest struct {
-	Items []string `json:"items"`
+type ingestMarketOrders struct {
+	Orders []string `json:"orders"`
+}
+
+type ingestMarketOrder struct {
+	ID string `jston:"Id"`
+	order_id=order_json['Id'],
+item_id=item_id,
+location_id=args['LocationId'],
+quality=order_json['QualityLevel'],
+enchantment=order_json['EnchantmentLevel'],
+price=price,
+amount=order_json['Amount'],
+expire=dateutil.parser.parse(order_json['Expires']),
+is_buy_order=is_buy_order,
 }
 
 func IngestMarketOrders(c *gin.Context) {
-	var incomingRequest ingestPostRequest
+	var incomingRequest ingestMarketOrders
 	c.BindJSON(&incomingRequest)
 
-	for _, v := range incomingRequest.Items {
+	for _, v := range incomingRequest.Orders {
 		work := dispatcher.Work{
 			Topic:   "market-order",
 			Message: []byte(v),
